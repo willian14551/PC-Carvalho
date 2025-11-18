59222f4e45c0f60f4d340bea3dbf5dc4cb6cf0d9
Kamilly <pkamillymattana@gmail.com>
2025-11-18 01:28:50 -0300

    Arrumei a "MainTesteMove"
    Alterei algumas coisas para melhor visualização

--- src/MainTesteMove.java
+++ src/MainTesteMove.java
@@ -7,37 +7,31 @@ public class MainTesteMove {
         String separador = ";";
 
         try (BufferedReader br = new BufferedReader(new FileReader(nomeArquivo))) {
-            System.out.println("==== LEGENDAS ====");
-            System.out.println("NN= NacionalNumber\n");
-
             // Lê a primeira linha do arquivo (cabeçalho)
             String cabecalho = br.readLine();
+
             if (cabecalho != null) {
                 String[] colunas = cabecalho.split(separador);
-                System.out.println("---------------------------------------------");
-                System.out.println("| " + String.join(" | ", colunas)+" |"); // Organiza as infos do cabeçalho
-                System.out.println("---------------------------------------------");
+                System.out.println("| " + String.join(" | ", colunas) + " |"); // Organiza as infos do cabeçalho
             }
-
             // Lê as linhas restantes do arquivo
             while ((linha = br.readLine()) != null) {
                 String[] dados = linha.split(separador); //Separa as strings em subStrings
-                // Dados da linha em um array de strings, cada elemento sendo um campo
-                System.out.println("| " + String.join(" | ", dados)+" |");
+                // Dados da linha em um array de strings, cada elemento é um campo
+                System.out.println("| " + String.join(" | ", dados) + " |");
             }
 
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("Erro: " + e.getMessage());
         }
 
 
-
-
         //        Move move = new Move("Bola de fogo", "Fogo", 30, 10);
-//        Move move2 = new Move("Jato de água", "Água", 25, 10);
-//
-//        move.movesPoke();
-//        move2.movesPoke();
+        //        Move move2 = new Move("Jato de água", "Água", 25, 10);
+        //
+        //        move.movesPoke();
+        //        move2.movesPoke();
 
     }
+
 }
