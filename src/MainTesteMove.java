8ab3a8f61f681b849508c3fc751a6aa9097476b0
Kamilly <pkamillymattana@gmail.com>
2025-11-18 15:08:01 -0300

    Novos testes e criação da classe "PokemonCsvLoader"
    - Criei a classe "PokemonCsvLoader" com seus atributos, métodos, setters, getters e construtores;
    - Testei os métodos da "PokemonCsvLoader".

--- src/MainTesteMove.java
+++ src/MainTesteMove.java
@@ -2,35 +2,10 @@ import java.io.*;
 
 public class MainTesteMove {
     public static void main(String[] args) {
-        String nomeArquivo = "src/pokedex.csv";
-        String linha;
-        String separador = ";";
+        PokemonCsvLoader csvLoader = new PokemonCsvLoader();
 
-        try (BufferedReader br = new BufferedReader(new FileReader(nomeArquivo))) {
-            // Lê a primeira linha do arquivo (cabeçalho)
-            String cabecalho = br.readLine();
+        csvLoader.lerCsvTotal("src/pokedex.csv");
 
-            if (cabecalho != null) {
-                String[] colunas = cabecalho.split(separador);
-                System.out.println("| " + String.join(" | ", colunas) + " |"); // Organiza as infos do cabeçalho
-            }
-            // Lê as linhas restantes do arquivo
-            while ((linha = br.readLine()) != null) {
-                String[] dados = linha.split(separador); //Separa as strings em subStrings
-                // Dados da linha em um array de strings, cada elemento é um campo
-                System.out.println("| " + String.join(" | ", dados) + " |");
-            }
-
-        } catch (IOException e) {
-            System.err.println("Erro: " + e.getMessage());
-        }
-
-
-        //        Move move = new Move("Bola de fogo", "Fogo", 30, 10);
-        //        Move move2 = new Move("Jato de água", "Água", 25, 10);
-        //
-        //        move.movesPoke();
-        //        move2.movesPoke();
 
     }
 
